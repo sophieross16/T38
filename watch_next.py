dca810bd13e2cafd395feb9994558f7f54f8ea06
SophieRoss <sophieross39@hotmail.com>
2023-02-17 15:35:52 +0000

    Compulsory Task 2 files

---
+++ watch_next.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 14:58:57 2023
+
+@author: sophieross
+"""
+
+# Compulsory Task 2
+import spacy
+nlp = spacy.load('en_core_web_md') # specifying the model we want to use. Remember to install this model by typing python -m spacy download en_core_web_md into your command line
+
+'''
+Input: description
+Output: title of the most similar movie
+Description: this function reads a list of movies and their descriptions from a txt file, then compares an inputted description to those in the file. It then outputs the movie, similarity value and corresponding description
+'''
+
+def movie_similarity(description):
+    # Initialising a nested list of movies and their description from the txt file
+    movies = []
+    with open('movies.txt','r') as movie_file:
+        for line in movie_file:
+            movie = line.split(':')
+            movies.append(movie)
+    # Initiliases a list to store the movie name and similarity value
+    similarities = []
+    # Compare inputted description to other descriptions in the nested list
+    for movie in movies:
+    # Applying NLP to the inputted and iterated description to find the similarities
+        iterated_description = nlp(movie[1])
+        similarity = nlp(description).similarity(iterated_description)
+    # Adding the movie, description and similarity value to the similarities list
+        similarity_val = [movie[0], movie[1], similarity]
+        similarities.append(similarity_val)
+    for index in similarities:
+    # Initialising the highest simlarity value as 0 in order to find the largest
+        highest_val = 0
+        if index[2] > highest_val:
+            highest_val = index[2]
+            highest_movie = index[0]
+            highest_description = index[1]
+    # Outputting results to the user
+    print(f'\n{highest_movie} is the most similar to your description, with a similarity value of {highest_val:.2f}.')
+    print(f'\nDescription: {highest_description}')
+
+# Testing function
+movie_similarity('Adapted from the bestselling novel by Madeleine St John, Ladies in Black is an alluring and tender-hearted comedy drama about the lives of a group of department store employees in 1959 Sydney.')
+    
